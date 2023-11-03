3ca4ba5d480d400f408100ec8085538f7d8bb226
specmate111 <akhilshaji519@gmail.com>
2023-11-03 15:57:46 +0530

    location picker

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,3 +1,4 @@
+import 'package:csc_picker/csc_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -21,6 +22,11 @@ class _LocationScreenState extends State<LocationScreen> {
   late bool _serviceEnabled;
   late PermissionStatus _permissionGranted;
   late LocationData _locationData;
+
+  String countryValue = "";
+  String stateValue = "";
+  String cityValue = "";
+  String address = "";
 
   Future<LocationData?> getLocation() async {
     _serviceEnabled = await location.serviceEnabled();
@@ -46,12 +52,136 @@ class _LocationScreenState extends State<LocationScreen> {
     return _locationData;
   }
 
-  
-
   @override
   Widget build(BuildContext context) {
+    showBottomScreen(context) {
+      showModalBottomSheet(
+          isScrollControlled: true,
+          enableDrag: true,
+          context: context,
+          builder: (context) {
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 25,
+                ),
+                AppBar(
+                  automaticallyImplyLeading: false,
+                  elevation: 1,
+                  backgroundColor: Colors.white,
+                  title: Row(
+                      //mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(
+                            Icons.clear,
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        const Text(
+                          'Location',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        )
+                      ]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(6)),
+                    child: SizedBox(
+                      height: 40,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Search your city',
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                          ),
+                          icon: Icon(Icons.search),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                ListTile(
+                  onTap: () {},
+                  horizontalTitleGap: 0.0,
+                  leading: Icon(
+                    Icons.my_location,
+                    color: Colors.blue,
+                  ),
+                  title: Text(
+                    'Use current location',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Enable Location',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.grey[300],
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 5, bottom: 5, top: 5),
+                    child: Text(
+                      'CHOOSE CITY',
+                      style: TextStyle(
+                        color: Colors.blueGrey[900],
+                      ),
+                    ),
+                  ),
+                ),
+
+                // Location picker
+
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: CSCPicker(
+                    layout: Layout.vertical,
+                    dropdownDecoration:
+                        const BoxDecoration(shape: BoxShape.rectangle),
+                    defaultCountry: CscCountry.India,
+                    onCountryChanged: (value) {
+                      setState(() {
+                        countryValue = value;
+                        print(countryValue);
+                      });
+                    },
+                    onStateChanged: (value) {
+                      setState(() {
+                        stateValue = value!;
+                      });
+                    },
+                    onCityChanged: (value) {
+                      setState(() {
+                        cityValue = value!;
+                        address = '$cityValue, $stateValue, $countryValue';
+                      });
+                      print(address);
+                    },
+                  ),
+                ),
+              ],
+            );
+          });
+    }
+
     return Scaffold(
       backgroundColor: AppColors.whiteColor,
+      resizeToAvoidBottomInset: true,
       body: Column(
         children: [
           Image.asset('assets/images/locatioon.png'),
@@ -125,14 +255,16 @@ class _LocationScreenState extends State<LocationScreen> {
             ),
           ),
           InkWell(
-            onTap: () {},
+            onTap: () {
+              showBottomScreen(context);
+            },
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
                 decoration:
                     BoxDecoration(border: Border(bottom: BorderSide(width: 2))),
                 child: Text(
-                  'Set locatikn manually',
+                  'Set location manually',
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                 ),
               ),
